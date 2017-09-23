fad72df9657495556931579136941d2878cfa06f
Min Kim <m-kim@kaist.ac.kr>
2017-09-23 23:18:58 +0900

    reading line by line

--- main.c
+++ main.c
@@ -3,21 +3,19 @@
 
 int main(int argc, char* argv[]){
 	
-	int i = 0;
 	int i=0;
 	FILE *file;
-	char c;
-		
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read; 
+//	char * pch;		
 	printf("%d\n",argc);
 
 	for( i; i<argc ;i++){
 		printf("%s",argv[i]);
 
 	}
-
-	printf("fucking c");
-
-		
+	
 	file = fopen("example1.s","r");
 
 	if(file == NULL) {
@@ -29,12 +27,18 @@ int main(int argc, char* argv[]){
 	file = fopen("./Project1_examples/example1.s","r");
 	if(file){
 		printf("in \n");
-		while ((c = fgetc(file))!=EOF){
-			printf("%c",c);
+		while ((read = getline(&line, &len,file))!=-1){
+		//	printf("len is: %zu \n ",len);
+			printf("Retrieved line of length %zu :\n",read);
+			printf("%s",line);
+		//	pch = strtok(line," ");
+		//	printf("%s", pch);
 		//	putchar(c);
 		}
 		fclose(file);
 	}	
+//	if (line)
+//		free(line);
 
 	printf("Hello World! \n");
 	return 0;
