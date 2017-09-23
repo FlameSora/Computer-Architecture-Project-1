a965929a563777091cafb93b54d37ac75dd1edfd
Min Kim <m-kim@kaist.ac.kr>
2017-09-23 22:24:42 +0900

    getting example name

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
 #include<stdio.h>
 
+
 int main(int argc, char* argv[]){
 	
+	int i;
+	FILE *file;
+	char c;	
+	printf("%d\n",argc);
+
+	for( i; i<argc ;i++){
+		printf("%s",argv[1]);
+
+	}
+		
+	file = fopen(argv[1],"r");
+	if(file){
+		printf("in \n");
+		while ((c = fgetc(file))!=EOF){
+			printf("%c",c);
+			putchar(c);
+		}
+		fclose(file);
+
+	}
+/*	if(file){
+		while((c = getc(file))!=EOF)
+			putchar(c);
+		fclose(file);
+	}
+*/		
 	printf("Hello World! \n");
 	return 0;
 }
